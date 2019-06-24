e86adffef230d41f1680c3ec26c1f041d392ac08
Vitor Santos <vitor7_sn@hotmail.com>
2019-06-24 18:53:23 -0300

    teste

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({extended: true, limit: '50mb',extend: true, parameterLimit:50000}));
+app.use(express.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({extended: true, limit: '50mb',extend: true, parameterLimit:50000}));
 
 mongoose
   .connect(
