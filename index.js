ba288f44f7874ec63055a6aafdc15f49a03d7a9d
Vitor Santos <vitor7_sn@hotmail.com>
2019-06-24 21:35:02 -0300

    teste

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 
-app.use(bodyParser.json({limit: '10mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+//app.use(bodyParser.json({limit: '10mb', extended: true}))
+//app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
  //app.use(bodyParser.urlencoded({extended: true, limit: '50mb',extend: true, parameterLimit:50000}));
  
