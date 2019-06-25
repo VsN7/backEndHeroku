9d6c3fc6c3c4510db82335d03b0533e8cc610199
Vitor Santos <vitor7_sn@hotmail.com>
2019-06-24 21:17:25 -0300

    teste

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const bodyParser = require('body-parser');
 
 // Iniciando o App
 const app = express();
-app.use(express.json());
+//app.use(express.json());
 app.use(cors());
 
 
 app.use(express.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({extended: true, limit: '50mb',extend: true, parameterLimit:50000}));
+
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+
+ //app.use(bodyParser.urlencoded({extended: true, limit: '50mb',extend: true, parameterLimit:50000}));
+ 
 
 mongoose
   .connect(
